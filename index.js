a356009b7112532ebfbec07cf1d99e4e5c9d3713
Joseph Thawn <pauthawn@Josephs-MacBook-Pro.local>
2022-07-31 16:32:10 -0500

    section-9

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log(reverse("awesome"));
 /**
  reverse("awesome")
     return reverse(str.slice(1)) + str[0];
-    return reverse("wesome") + ("a")
+    return reverse("wesome") + "a"
 
 reverse("wesome")
     return reverse(str.slice(1)) + str[0];
@@ -39,15 +39,101 @@ reverse("me")
 reverse("e")
   return reverse(str.slice(1)) + str[0];
   return reverse("") + "e"
-      
+  since str.length <= 1 base case stops and the result return "e"
 
-//Will be back for this explanation using for video lesson tutrial
+reverse("me")
+  return reverse("e") + "m
+  return "e" + "m" + "em"
 
+reverse("ome")
+  return reverse("me") + ("o")
+  return "em" + "o" = "emo"
+
+reverse("some")
+    return reverse("ome") + ("s")
+    return "emo" + "s" = "emos"
+
+reverse("esome")
+    return reverse("some") + ("e")
+    return "emos" + "e" = "emose"
+
+reverse("wesome")
+  return reverse('esome') + ('w')
+  return "emose" + "w" = "emosew"
+
+reverse("awesome")
+    return reverse("wesome") + "a"
+    return "emosew" + "a" = "emosewa"
+
+    the final result is "emosewa"
 
 
  
 
 
+
+
+
+
+
+
+
+//Will be back for this explanation using for video lesson tutrial
+
+ */
+
+//Different solution from YouTube
+
+function reverseOne(str) {
+  if (str.length === 0) return "";
+  return reverseOne(str.slice(1)) + str[0];
+}
+console.log(reverseOne("happy"));
+/**
+reverseOne('happy)
+  return reverseOne(str.slice(1) + str[0]
+  return reverseOne("appy") + 'h'
+
+reverseOne("appy")
+  return reverseOne(str.slice(1) + str[0]
+  return reverseOne("ppy") + ("a")
+
+reverseOne("ppy")
+  return reverseOne(str.slice(1) + str[0]
+  return reverseOne("py") + "p"
+
+reverseOne("py")
+  return reverseOne(str.slice(1) + str[0]
+  return reverseOne("y") + "p"
+
+reverseOne("y")
+  return reverseOne(str.slice(1) + str[0])
+  return reverseOne("")  + 'y'
+  since str.length == 0 base case stops and the result return y
+
+  reverseOne("py")
+    return reverseOne("y") + "p"
+    return "y" = 'p" ="yp"
+
+  reverseOne("ppy")
+    return reverseOne("py") + "p"
+    return "yp" + "p" = "ypp"
+
+  reverseOne("appy")
+    return reverseOne("ppy") + ("a")
+    return "ypp" + "a" = "yppa"
+
+  reverseOne('happy)
+    return reverseOne("appy") + 'h'
+    return "yppa" + "h" = "yppah"
+
+  The final answer is "appyah"
+
+
+
+
+
+ 
 
 
  */
